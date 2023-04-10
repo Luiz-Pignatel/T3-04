c24c332a42af98eb19788cc964ea8b6d1ff945f9
Luiz-Pignatel <126469610+Luiz-Pignatel@users.noreply.github.com>
2023-04-10 09:00:16 -0300

    Update Atividade24.java

--- Atividade24.java
+++ Atividade24.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 public class Atividade24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num;
+        int num, quantNumeros;
         String continua = "S";
 
         while (!continua.equalsIgnoreCase("N")) {
-            System.out.print("Digite numero: ");
+            System.out.print("Digite a quantidade de numeros a serem digitados: ");
+            quantNumeros = input.nextInt();
 
-            while (!input.hasNextInt()) {
-                System.out.print("Digite apenas numeros: ");
-                input.next();
+            for (int i = 0; i < quantNumeros; i++) {
+                System.out.print("Digite numero: ");
+
+                while (!input.hasNextInt()) {
+                    System.out.print("Digite apenas numeros: ");
+                    input.next();
+                }
+                num = input.nextInt();
+
+                input.nextLine();   // consome o buffer/enter
+
+                System.out.printf("O numero %d e ", num);
+                if (num > 0) {
+                    System.out.println("positivo");
+                } else if (num < 0) {
+                    System.out.println("negativo");
+                } else {
+                    System.out.println("neutro");
+                }
             }
-            num = input.nextInt();
-            input.nextLine(); // consome o buffer/enter
-
-            System.out.printf("O numero %d e ", num);
-            if (num > 0) {
-                System.out.println("positivo");
-            } else if (num < 0) {
-                System.out.println("negativo");
-            } else {
-                System.out.println("neutro");
-            }
-
+            
             System.out.print("Deseja continuar? [S/N] ");
             continua = input.nextLine();
         }
